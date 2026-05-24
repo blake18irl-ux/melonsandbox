5a7d0518236336bc237e60010afd180dc42169e5
blake18irl-ux <blake18irl@gmail.com>
2026-05-23 22:01:08 -0400

    Implement global error handling component

--- _next/static/chunks/app/global-error-6475929ef6cf609e.js
+++ _next/static/chunks/app/global-error-6475929ef6cf609e.js
@@ -1,1 +1,41 @@
-
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[4219], {
+    10583: (e, n, r) => {
+        Promise.resolve().then(r.bind(r, 54062))
+    }
+    ,
+    54062: (e, n, r) => {
+        "use strict";
+        r.r(n),
+        r.d(n, {
+            default: () => l
+        });
+        var s = r(95155)
+          , t = r(12115);
+        function l(e) {
+            let {error: n, reset: r} = e;
+            return (0,
+            t.useEffect)( () => {
+                console.error(n)
+            }
+            , [n]),
+            (0,
+            s.jsx)("html", {
+                children: (0,
+                s.jsxs)("body", {
+                    children: [(0,
+                    s.jsx)("h2", {
+                        children: "Something went wrong!"
+                    }), (0,
+                    s.jsx)("button", {
+                        onClick: () => r(),
+                        children: "Try again"
+                    })]
+                })
+            })
+        }
+    }
+}, e => {
+    e.O(0, [8441, 1255, 7358], () => e(e.s = 10583)),
+    _N_E = e.O()
+}
+]);
